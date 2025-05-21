8d181098be9093499d06f685948e5d99b834b99e
PoloBustillo <39663418+PoloBustillo@users.noreply.github.com>
2025-05-20 23:27:04 -0600

    add more pages

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,56 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
 });
 
-export const collections = { blog };
+const computacion = defineCollection({
+  // Load Markdown and MDX files in the `src/content/computacion/` directory.
+  loader: glob({ base: "./src/content/computacion", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+
+const fisica = defineCollection({
+  loader: glob({ base: "./src/content/fisica", pattern: "**/*.{md,mdx}" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+
+const fisicoQuimica = defineCollection({
+  loader: glob({
+    base: "./src/content/fisico-quimica",
+    pattern: "**/*.{md,mdx}",
+  }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+
+export const collections = { blog, computacion, fisica, fisicoQuimica };
--- src/lib/wp.ts
+++ src/lib/wp.ts
@@ -5,7 +5,7 @@ export const getPages = async () => {
   if (!domain) {
     throw new Error("Please set the WP_DOMAIN environment variable");
   }
-  const res = await fetch(`${apiUrl}`, {
+  const res = await fetch(`${apiUrl}/pages`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -13,13 +13,34 @@ export const getPages = async () => {
     },
     cache: "no-store",
   });
+
   if (!res.ok) throw new Error("Failed to fetch pages");
-  console.log("Pages", res.headers.get("content-type"));
   const contentType = res.headers.get("content-type");
-  //   if (!contentType || !contentType.includes("application/json")) {
-  //     throw new Error("Expected JSON response but received something else");
-  //   }
-  const data = await res.text();
-  console.log("Pages", data);
+  if (!contentType || !contentType.includes("application/json")) {
+    throw new Error("Expected JSON response but received something else");
+  }
+  const data = await res.json();
+  return data;
+};
+
+export const getPage = async (slug: string) => {
+  if (!domain) {
+    throw new Error("Please set the WP_DOMAIN environment variable");
+  }
+  const res = await fetch(`${apiUrl}/pages?slug=${slug}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Failed to fetch pages");
+  const contentType = res.headers.get("content-type");
+  if (!contentType || !contentType.includes("application/json")) {
+    throw new Error("Expected JSON response but received something else");
+  }
+  const data = await res.json();
   return data;
 };
